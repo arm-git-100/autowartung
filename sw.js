d9f5ce70bb440f014c9850f56f51176553b20faa
arm-git-100 <a@armza.de>
2026-05-22 16:33:39 +0200

    Reparaturen: Werkstatt+Kosten, mehrzeilige Beschreibungen, SoH-Historie, Cache v5
    - Reparaturen: neue Felder Werkstatt + Kosten (€) inkl. PDF-Spalten + Summe
    - Reparatur-Beschreibung und Kundendienst-Was-wurde-gemacht jetzt mehrzeilig (textarea)
    - Stammdaten: SoH als Historie mit mehreren Einträgen (Wert + Datum) statt Einzelwert; Migration alter Daten
    - SW-Cache auf v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'autowartung-v4';
+const CACHE = 'autowartung-v5';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', e => {
